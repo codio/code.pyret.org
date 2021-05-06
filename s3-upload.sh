d7b93cea34551403e13f06ce8ff346dc7d96a359
Andrey Nikitin <anikitin@codio.com>
2021-05-06 14:38:41 +0300

    local address for scripts

--- s3-upload.sh
+++ s3-upload.sh
@@ -6,7 +6,7 @@ s3_assets_access_secret=$2
 git_hash=$(git rev-parse HEAD)
 
 upload() {
-  node uploadToS3.js pyret/${git_hash} build "${s3_assets_access_key}" "${s3_assets_access_secret}"
+  node uploadToS3.js pyret/${git_hash} build/web "${s3_assets_access_key}" "${s3_assets_access_secret}"
 }
 
 upload
